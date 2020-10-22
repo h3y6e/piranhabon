9e6c31f3e1fc7435b8369d9c694e8023376f2951
5ebec <hash.a5ebec@gmail.com>
2020-10-22 14:58:35 +0900

    killed a global variable

--- src/main.py
+++ src/main.py
@@ -7,8 +7,6 @@ from src.Calender import Calender
 from dotenv import load_dotenv
 load_dotenv()
 
-colorId = 0
-
 
 def get_retry(url, retry_times=5):
     for t in range(retry_times + 1):
@@ -24,7 +22,7 @@ def get_retry(url, retry_times=5):
 
 
 def main():
-    global colorId
+    colorId = 0
     calender = Calender()
     base_url = "https://subjregist.naist.jp/"
 
@@ -35,37 +33,33 @@ def main():
         "data[User][account]": os.environ['USER_ACCOUNT'],
         "data[User][password]": os.environ['USER_PASSWORD']
     }
-
     res = session.post(base_url, data=data)
     res.raise_for_status()
-
-    # delete all events first
-    calender.delete_all_events()
 
     # get registered subjects list
     list_bs = BeautifulSoup(res.content, "html.parser")
     subjects = list_bs.select('.tbl01.mB20 a[target=_blank]')
-
     print(subjects)
+
+    # delete all events in google calendar
+    calender.delete_all_events()
 
     # run export for each subjects
     for subject in subjects:
+        # get schedules
         subject_name = subject.text
         subject_url = subject.get('href')
         print(subject_name, subject_url)
         subject_response = get_retry(subject_url)
         subject_bs = BeautifulSoup(subject_response.content, "html.parser")
+        item = 5 if "subjects" in subject_url else 4
+        schedules = subject_bs.select('.tbl01.mB20')[item].select('tr td')
 
-        # one class
-        if "subjects" in subject_url:
-            col = 5
-        # more than one class
-        else:
-            col = 4
-        schedules = subject_bs.select('.tbl01.mB20')[col].select('tr td')
-
+        # skip if no schedule is defined
         if len(schedules) == 1:
             continue
+
+        # add schedules to google calendar
         calender.add_schedules(subject_name, subject_url, schedules, colorId)
         sleep(1)
 
